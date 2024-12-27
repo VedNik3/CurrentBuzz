d8e0058888bfb7456714f591cddaee54483a7f49
VedNik3 <vedantnikam812@gmail.com>
2024-12-27 21:23:41 +0530

    added backend link

--- Client/src/utils/constanta.js
+++ Client/src/utils/constanta.js
@@ -1,2 +1,4 @@
-export const API_END_POINT_USER = "http://localhost:5000/api/user"
-export const API_END_POINT_FRONTPAGE = "http://localhost:5000/api/frontpage"
+// export const API_END_POINT_USER = "http://localhost:5000/api/user"
+// export const API_END_POINT_FRONTPAGE = "http://localhost:5000/api/frontpage"
+export const API_END_POINT_USER = "https://currentbuzz.onrender.com/api/user"
+export const API_END_POINT_FRONTPAGE = "https://currentbuzz.onrender.com/api/frontpage"
